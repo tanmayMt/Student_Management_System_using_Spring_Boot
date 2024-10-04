c5fce7a6872cde19e18a0a211d7785342f619adf
web-collab <tanmoy587@gmail.com>
2024-10-04 10:38:54 +0530

    Create Interface ‘JpaRepository’ in ‘repository’ directory and extends ‘JpaRepository’

---
+++ src/main/java/com/tanmay/studentmanagement/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.tanmay.studentmanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tanmay.studentmanagement.model.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Long>{
+
+}
